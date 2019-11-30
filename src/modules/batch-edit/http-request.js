9b8ffbe6f31e8f82734da5315902fc83dce334bb
Oleh <oleh.buhaienko@chatbots.studio>
2019-11-30 16:24:49 +0200

    Add eslint Set up private properties

--- src/modules/batch-edit/http-request.js
+++ src/modules/batch-edit/http-request.js
@@ -1,80 +1,75 @@
 const axios = require('axios');
 
-/****
+/** **
  * HttpRequest - class, wrapping axios method to add logic of retries
  * and easier request status handling
  */
-class HttpRequest
-{
-    /**
+class HttpRequest {
+  /**
      *
      * @param url - String, accepts the query url with already parsed url path parameters
      * @param verb - String [GET, POST, PUT, DELETE], request method
      * @param body - Object, key-value pairs of JSON request body
      */
-    constructor({
-        url,
-        verb,
-        body
-    },{
-       maxRetries,
-    }) {
-        this._url = url;
-        this._verb = verb;
-        this._body = body;
-        this.finished = false;
-        this.started = false;
-        this.result = {};
-        this.numRetries = 0;
-        this.maxRetries = 2;
-    }
+  constructor({
+    url,
+    verb,
+    body,
+  }, {
+    maxRetries,
+  }) {
+    this.url = url;
+    this.verb = verb;
+    this.body = body;
+    this.finished = false;
+    this.started = false;
+    this.result = {};
+    this.numRetries = 0;
+    this.maxRetries = maxRetries;
+  }
 
-    /**
+  /**
      * Method to run query itself
      * @returns {Promise<any>} - resolves with ref to object context once query fullfilled
      *                           successfully or with error
      */
-    run() {
-        return new Promise(async (resolve, reject) => {
-            this.started = true;
-            this.result = null;
-            this.numRetries++;
-            try {
-                const result = await axios({
-                    method: this._verb,
-                    url: this._url,
-                    data: this._body
-                });
-                this.result = {
-                    success: true,
-                    numRetry: this.numRetries,
-                    data: result.data,
-                }
-            } catch(e) {
-                if(this.numRetries >= this.maxRetries)
-                {
-                    this.result = {
-                        success: false,
-                        numRetry: this.numRetries,
-                        data: (e.response && {
-                            code: e.response.status,
-                            text: e.response.statusText,
-                        }) || {
-                            code: e.errno,
-                        }
-                    }
-                }
-                else
-                    this.started = false;
-
-            } finally {
-                if(this.result) {
-                    this.finished = true;
-                    return resolve(this);
-                }
-            }
-        })
-    }
+  run() {
+    return new Promise(async (resolve) => {
+      this.started = true;
+      this.result = null;
+      this.numRetries++;
+      try {
+        const result = await axios({
+          method: this.verb,
+          url: this.url,
+          data: this.body,
+        });
+        this.result = {
+          success: true,
+          numRetry: this.numRetries,
+          data: result.data,
+        };
+      } catch (e) {
+        if (this.numRetries >= this.maxRetries) {
+          this.result = {
+            success: false,
+            numRetry: this.numRetries,
+            data: (e.response && {
+              code: e.response.status,
+              text: e.response.statusText,
+            }) || {
+              code: e.errno,
+            },
+          };
+        } else this.started = false;
+      } finally {
+        if (this.result) {
+          this.finished = true;
+        }
+      }
+      return resolve(this);
+    });
+  }
 }
 
 module.exports = HttpRequest;
--- src/modules/batch-edit/index.js
+++ src/modules/batch-edit/index.js
@@ -1,23 +1,18 @@
 const BatchEditor = require('./batch-editor');
 
 module.exports = {
-    bulkActions: ({endpoint, payloads}) => {
-        if(!endpoint)
-            throw new Error('Endpoint object should be specified');
+  bulkActions: ({ endpoint, payloads }) => {
+    if (!endpoint) throw new Error('Endpoint object should be specified');
 
-        if(!endpoint.url)
-            throw new Error('Endpoint path should be specified');
+    if (!endpoint.url) throw new Error('Endpoint path should be specified');
 
-        if(!endpoint.verb)
-            throw new Error('Endpoint verb should be specified');
+    if (!endpoint.verb) throw new Error('Endpoint verb should be specified');
 
-        const BatchEditRequests = new BatchEditor({endpoint, payloads},{
-            maxRequests: process.env.REQUESTS_LIMIT_AMOUNT || 5,
-            maxRequestsTimeUnit: process.env.REQUESTS_LIMIT_TIMEUNIT || 10,
-            maxRetries: process.env.MAX_RETRIES || 2,
-        });
-
-        return BatchEditRequests.doActions();
-
-    },
-}
+    const BatchEditRequests = new BatchEditor({ endpoint, payloads }, {
+      maxRequests: process.env.REQUESTS_LIMIT_AMOUNT || 5,
+      maxRequestsTimeUnit: process.env.REQUESTS_LIMIT_TIMEUNIT || 10,
+      maxRetries: process.env.MAX_RETRIES || 2,
+    });
+    return BatchEditRequests.doActions();
+  },
+};
